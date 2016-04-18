9bb7518184de39b0a38882cdea0ee199af85588f
idoctnef <idoctnef@openmailbox.org>
2016-04-18 18:12:30 +0200

    començant a passar-ho a bootstrap

--- www/functions.js
+++ www/functions.js
@@ -31,20 +31,50 @@ function OpenPage(linkgiv){
 
 function DisplayCasalsList(casalsListFromServer){
   var cl; //CasalsList
+  var itemrow;
   cl=casalsListFromServer;
+  itemrow=0;
 
   document.getElementById("espaiBoxCasalListCasals").innerHTML="";
   content="";
+  content+="<div class='row'>";
+  /*
+  content+="";
+  */
   for(var i=0; i<cl.length; i++)
   {
+      if(itemrow>2)
+      {
+          itemrow=0;
+          content+="</div>";
+          content+="<div class='row'>";
+          content+="";
+          content+="";
+      }
+      content+="<div id='"+cl[i].name+"_Box' class='col-md-4 portfolio-item'>";
+      content+="<a href='#'>";
+      content+="<img class='img-responsive' src='http://placehold.it/700x400' alt=''>";
+      content+="</a>";
+      content+="<h3>";
+      content+="<a href='#'>"+cl[i].name+"</a>";
+      content+="</h3>";
+      content+="<p>"+cl[i].descr+"</p>";
+      content+="<p>"+cl[i].web+"</p>";
+      content+="<p>"+cl[i].dir+"</p>";
+      content+="</div>";
+      itemrow++;
+
+
+    /*versio anterior, sense bootstrap
     content+="<div id='"+cl[i].name+"_Box' class='boxCasalListCasals'>";
     content+="<h1 onClick='OnCasalFromListCasalsClick(this.id);'>"+cl[i].name+"</h1>";
     content+="<img class='imatgeCasalListCasals' src='imatgestemp/fotocasal1.png' onClick='OnCasalFromListCasalsClick(this.id);' />";
     content+="<h2>"+cl[i].descr+"</h2>";
     content+="<h3><a href='"+cl[i].web+"' target='_blank'>"+cl[i].web+"</a></h3>";
     content+="<h4>"+cl[i].dir+"</h4>";
-    content+="</div>";
+    content+="</div>";*/
   }
+  content+="</div>";
   document.getElementById("espaiBoxCasalListCasals").innerHTML=content;
 
 }
