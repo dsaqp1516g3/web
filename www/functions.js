4e9008a59f1184fcb74ffaf607953d6a2e4e4f4d
idoctnef <idoctnef@openmailbox.org>
2016-04-20 22:16:56 +0200

    fent el js del casal.html, pinta la info de l'json a l'html, falta ajustar q agafi el json del restful

--- www/functions.js
+++ www/functions.js
@@ -1,41 +1,6 @@
 
-function DisplayAlertEmergency(){
-  alert("+info");
-}
-
-/* funcions OnLoad */
-function OnLoadDefault(){
-  /* les funcions q s'executen casi sempre, agrupades */
-    EmergencyBar();
-}
-function OnLoadIndex(){
-    OnLoadDefault();
-}
-/* funcions de llistacasals.html */
-function OnLoadLlistacasals(){
-  OnLoadDefault();
-  casalsListFromServer = getCasalsList();
-/*  alert(casalsListFromServer);
-  DisplayCasalsList(casalsListFromServer);*/
-}
-function OnLoadLlistaEvents(){
-  OnLoadDefault();
-  casalsListFromServer = getCasalsList();
-/*  alert(casalsListFromServer);
-  DisplayCasalsList(casalsListFromServer);*/
-}
-function OnLoadCasal(){
-  OnLoadDefault();
-  // aquí va la crida a la funció que agafa del sessionstorage la uri on anar a buscar la info del casal
-}
-function OnLoadSignIn(){
-  document.body.style.backgroundImage = "url('images/backgroundSignin1.jpg')";
-  document.body.style.backgroundSize = "cover";
-  document.body.style.overflow= "hidden";
-}
-
-
-
+/* ---------------------------------------EMERGÈNCIES-----------------------------
+barra emergències */
 function EmergencyBar(){
   document.getElementById("emergencydiv").innerHTML="";
   content="";
@@ -44,10 +9,46 @@ function EmergencyBar(){
   content+="<h3>Concentració 20h pl.Revolució</h3>";
   document.getElementById("emergencydiv").innerHTML=content;
 }
+function DisplayAlertEmergency(){
+  toastr.info("funcionalitat encara no disponible");
+}
+
+/* ---------------------------------------DEFAULT-----------------------------
+lu que s'executa casi sempre a la majoria de les pàgines
+les funcions q s'executen casi sempre, agrupades */
+function OnLoadDefault(){
+    EmergencyBar();
+}
+
+
+/* ---------------------------------------INDEX-----------------------------
+funcions de
+index.html
+*/
+function OnLoadIndex(){
+    OnLoadDefault();
+}
+
+
+
+
+
+
 function OpenPage(linkgiv){
   window.open (linkgiv,'_self',false)
 }
 
+
+/* -----------------------------LLISTACASALS---------------------------------------
+funcions de la pàgina
+llistacasals.html
+*/
+function OnLoadLlistacasals(){
+  OnLoadDefault();
+  casalsListFromServer = getCasalsList();
+/*  alert(casalsListFromServer);
+  DisplayCasalsList(casalsListFromServer);*/
+}
 function DisplayHtmlCasalsList(casalsListFromServer){
   var cl; //CasalsList
   var itemrow;
@@ -85,4 +86,63 @@ function DisplayHtmlCasalsList(casalsListFromServer){
 }
 function OnCasalFromListCasalsClick(idcasal){
   alert("això al clicar et porta a la pàgina del casal");
+}
+
+/* -----------------------------------CASAL---------------------------------
+funcions de la pàgina
+casal.html
+*/
+function OnLoadCasal(){
+  OnLoadDefault();
+  // aquí va la crida a la funció que agafa del sessionstorage la uri on anar a buscar la info del casal
+
+//  getCasalById(idCasal); <-- funcio q encara no està, és la que pilla del restful
+          /* mentre no tenim la api a punt, poso l'objecte hardcoded */
+            var objCasal={
+              description: "El Ateneu La Porka es un centro social okupado y autogestionado en una sucursal bancaria abandonada de Caja Madrid del barrio de Sant Antoni de Barcelona.",
+              web: "www.laporka.org",
+              direccio: "c/ Av.Mistral 33 baixos",
+              mail: "ateneulaporka@riseup.net"
+            };
+            /* quan tinguem la api a punt, s'elimina això, i s'afegeix la crida a la funció q pilla el data del restful */
+
+  DisplayHTMLCasal(objCasal);
+}
+function DisplayHTMLCasal(oC){
+  /* 'oC' és la variable objecte on va tota la info del casal */
+
+
+  document.getElementById("casalDescription").innerHTML=oC.description;
+  document.getElementById("casalWeb").innerHTML=oC.web;
+  document.getElementById("casalDireccio").innerHTML=oC.direccio;
+  document.getElementById("casalMail").innerHTML=oC.mail;
+  // aqí a partir de les coordenades es monta la url a la api q mostra el lloc
+  document.getElementById("casalDescription").src="http://www.openstreetmap.org/export/embed.html?bbox=2.1449947357177734%2C41.371849151666204%2C2.1689414978027344%2C41.38136509656854&amp;layer=mapnik&amp;marker=41.37659924742821%2C2.156968116760254";
+
+  document.getElementById("casalDescription").innerHTML=oC.description;
+}
+
+
+/* ---------------------------------LLISTAEVENTS-----------------------------------
+funcions de la pàgina
+llistaevents.html
+*/
+function OnLoadLlistaEvents(){
+  OnLoadDefault();
+  casalsListFromServer = getCasalsList();
+/*  alert(casalsListFromServer);
+  DisplayCasalsList(casalsListFromServer);*/
+}
+
+/* ------------------------------------SIGNIN--------------------------------
+funcions de la pàgina
+signin.html
+*/
+function OnLoadSignIn(){
+  document.body.style.backgroundImage = "url('images/backgroundSignin1.jpg')";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.overflow= "hidden";
+}
+function OnSubmitSignin(){
+  toastr.warning("encara no disponible");
 }
